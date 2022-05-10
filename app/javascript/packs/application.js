f11e733b854e9fd2c5a60a1365227f510c9c99ad
Guilherme Silva <gfsilva@hotmail.com>
2022-05-10 20:47:58 -0300

    rev

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,7 +8,7 @@ import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 import { loadDynamicBannerText } from '../components/banner';
-import { obras } from '../components/banner';
+
 
 
 Rails.start()
